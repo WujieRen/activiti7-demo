92837441f867f26f1c920b02cb747f7b740062f9
wujieren <rwj005@outlook.com>
2022-10-05 14:22:13 +0800

    测试流程定义

--- src/test/java/cn/rwj/demo/activiti7/Part1Deployment.java
+++ src/test/java/cn/rwj/demo/activiti7/Part1Deployment.java
@@ -31,7 +31,9 @@ public class Part1Deployment {
      *          1.实现 UserDetailsService 接口，重写 loadUserByUsername 方法
      * 部署流程导致：
      *      1. act_re_deployment表，部署，每一次部署，新增一条记录
-     *      2. act_re_procdef表，流程定义表
+     *      2. act_re_procdef表，流程定义表。每次部署时，如果key不变，则增加版本号，表示是同一个流程；
+     *          但是流程定义是可变的，即可以修改流程的内容。如：以前是超过半小时迟到扣钱，现在是弹性上下班。流程内容变了，只要key不变，那就代表是同一个流程。
+     *          这里的key对应表中的key字段，值为BPMN流程的id： <process id="part1_deployment" name="流程定义名称" isExecutable="true">
      *      3. act_ge_bytearray表，存储bpmn文件内容
      */
     @Test
@@ -42,6 +44,7 @@ public class Part1Deployment {
                 .addClasspathResource(filename)
                 //.addClasspathResource(pngname)//图片
                 .name("测试流程部署BPMN文件")
+                .key("p1")
                 .deploy();
         System.out.println(deployment.getName());
     }
---
+++ src/test/java/cn/rwj/demo/activiti7/Part2ProcessDefinition.java
@@ -0,0 +1,53 @@
+package cn.rwj.demo.activiti7;
+
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.repository.ProcessDefinition;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * @Author rwj
+ * @Date 2022/10/5
+ * @Description
+ */
+@SpringBootTest
+public class Part2ProcessDefinition {
+
+    @Resource
+    RepositoryService repositoryService;
+
+    //查询流程定义
+    @Test
+    public void getDefinitions(){
+        List<ProcessDefinition> list = repositoryService.createProcessDefinitionQuery()
+                .list();
+        for(ProcessDefinition pd : list){
+            System.out.println("------流程定义--------");
+            System.out.println("Name："+pd.getName());
+            System.out.println("Key："+pd.getKey());
+            System.out.println("ResourceName："+pd.getResourceName());
+            System.out.println("DeploymentId："+pd.getDeploymentId());
+            System.out.println("Version："+pd.getVersion());
+
+        }
+
+    }
+
+    /**
+     * 删除流程定义
+     * 1. act_re_deployment表，删除 id=4fcb10eb-446f-11ed-b9a6-a036bc09649b 的记录
+     * 2. act_re_procdef表，删除 deployment_id=4fcb10eb-446f-11ed-b9a6-a036bc09649b 的记录
+     * 3. act_ge_bytearray表，删除 deployment_id=4fcb10eb-446f-11ed-b9a6-a036bc09649b 的记录
+     */
+    @Test
+    public void delDefinition(){
+
+        String pdID="4fcb10eb-446f-11ed-b9a6-a036bc09649b";
+        repositoryService.deleteDeployment(pdID,true);
+        System.out.println("删除流程定义成功");
+    }
+
+}
