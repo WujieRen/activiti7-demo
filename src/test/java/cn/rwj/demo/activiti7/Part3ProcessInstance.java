a0f050f610b5f25595b73952eaabd90d5dd057c4
wujieren <rwj005@outlook.com>
2022-10-06 14:22:02 +0800

    测试新特性TaskRuntime

--- src/test/java/cn/rwj/demo/activiti7/Part3ProcessInstance.java
+++ src/test/java/cn/rwj/demo/activiti7/Part3ProcessInstance.java
@@ -21,21 +21,20 @@ public class Part3ProcessInstance {
 
     /**
      * 初始化流程实例
-     *     1. act_ru_execution表，表运行时流程执行实例
-     *          初始化时会生成两条数据，一个是开始节点的，一个是开始节点随后节点的.
-     *          后续执行到那个节点就会使那个节点的数据，执行到那里就是哪里，之行结束就没了。
-     *     2. act_ru_task表，运行时任务节点
-     *     3. act_hi_actinst表，执行历史
-     *     4. act_hi_procinst表，流程实例历史
-     *     5. act_hi_taskinst表，任务实例历史
+     * 1. act_ru_execution表，表运行时流程执行实例
+     * 初始化时会生成两条数据，一个是开始节点的，一个是开始节点随后节点的.
+     * 后续执行到那个节点就会使那个节点的数据，执行到那里就是哪里，之行结束就没了。
+     * 2. act_ru_task表，运行时任务节点
+     * 3. act_hi_actinst表，执行历史
+     * 4. act_hi_procinst表，流程实例历史
+     * 5. act_hi_taskinst表，任务实例历史
      *     TODO: 另外，看有些博客和课程说到 act_ru_identitylink 表也会有变化，我这里没发现有变化，后面用NPMNJS测试了再补充。
      *          我用的 7.1.0.M6 版本和 Acti BPMN Visualizer 插件。
      *     解决上次提交疑问，我这里提交没有在 act_ru_identitylink 表生成记录是因为我没有给  UserTask 设置 Asignee。
      *     6. act_ru_identitylink表，从该表中可根据流程实例ID查询 user_id，group_id，type等
-     *
      */
     @Test
-    public void initProcessInstance(){
+    public void initProcessInstance() {
         //1、获取页面表单填报的内容，请假时间，请假事由，String formData
         //2、formData 写入业务表，返回业务表主键ID==businessKey
         //3、把业务数据与Activiti7流程数据关联
@@ -43,21 +42,21 @@ public class Part3ProcessInstance {
 //        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("part4_task_1","businessKey_p4");
 //        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("part4_task_claim","businessKey_p4_claim");
 //        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("part6_uel_v2","businessKey_p6_uel");
-        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("part6_uel_v3","businessKey_p6_uel");
-//        ProcessInstance start = runtimeService.createProcessInstanceBuilder().name("test").processDefinitionKey("part6_uel_v3").businessKey("businessKey_p6_uel").start();
-        System.out.println("流程实例ID："+processInstance.getProcessDefinitionId());
+//        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("part6_uel_v3","businessKey_p6_uel");
+        ProcessInstance processInstance = runtimeService.createProcessInstanceBuilder().name("测试新特性TaskRuntime").processDefinitionKey("part8_processruntime").businessKey("businessKey_p8").start();
+        System.out.println("流程实例ID：" + processInstance.getProcessDefinitionId());
     }
 
     //获取流程实例列表
     @Test
-    public void getProcessInstances(){
+    public void getProcessInstances() {
         List<ProcessInstance> list = runtimeService.createProcessInstanceQuery().list();
-        for(ProcessInstance pi : list){
+        for (ProcessInstance pi : list) {
             System.out.println("--------流程实例------");
-            System.out.println("ProcessInstanceId："+pi.getProcessInstanceId());
-            System.out.println("ProcessDefinitionId："+pi.getProcessDefinitionId()); //从这里可以看出，每次部署Deployment，如果key不变，deployment会增加版本，且启动实例时用的是该key对应的最新版本的流程
-            System.out.println("isEnded"+pi.isEnded());
-            System.out.println("isSuspended："+pi.isSuspended());
+            System.out.println("ProcessInstanceId：" + pi.getProcessInstanceId());
+            System.out.println("ProcessDefinitionId：" + pi.getProcessDefinitionId()); //从这里可以看出，每次部署Deployment，如果key不变，deployment会增加版本，且启动实例时用的是该key对应的最新版本的流程
+            System.out.println("isEnded" + pi.isEnded());
+            System.out.println("isSuspended：" + pi.isSuspended());
 
         }
         /*List<ProcessInstance> list = runtimeService.createProcessInstanceQuery().processInstanceId("97b1cfe7-4492-11ed-908f-a036bc09649b").list();
@@ -68,7 +67,7 @@ public class Part3ProcessInstance {
 
     //暂停与激活流程实例
     @Test
-    public void activitieProcessInstance(){
+    public void activitieProcessInstance() {
         runtimeService.suspendProcessInstanceById("97b1cfe7-4492-11ed-908f-a036bc09649b");
         System.out.println("挂起流程实例");
         ProcessInstance processInstance = runtimeService.createProcessInstanceQuery().processInstanceId("97b1cfe7-4492-11ed-908f-a036bc09649b").list().get(0);
@@ -83,8 +82,8 @@ public class Part3ProcessInstance {
 
     //删除流程实例
     @Test
-    public void delProcessInstance(){
-        runtimeService.deleteProcessInstance("97b1cfe7-4492-11ed-908f-a036bc09649b","测试删除");
+    public void delProcessInstance() {
+        runtimeService.deleteProcessInstance("97b1cfe7-4492-11ed-908f-a036bc09649b", "测试删除");
         System.out.println("删除流程实例");
     }
 
---
+++ src/test/java/cn/rwj/demo/activiti7/Part9TaskRuntime.java
@@ -0,0 +1,67 @@
+package cn.rwj.demo.activiti7;
+
+import cn.rwj.demo.activiti7.secuexam.SecurityUtil;
+import org.activiti.api.runtime.shared.query.Page;
+import org.activiti.api.runtime.shared.query.Pageable;
+import org.activiti.api.task.model.Task;
+import org.activiti.api.task.model.builders.TaskPayloadBuilder;
+import org.activiti.api.task.runtime.TaskRuntime;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * @Author rwj
+ * @Date 2022/10/6
+ * @Description
+ */
+@SpringBootTest
+public class Part9TaskRuntime {
+
+    @Resource
+    private SecurityUtil securityUtil;
+
+    @Resource
+    private TaskRuntime taskRuntime;
+
+    //获取当前登录用户任务
+    @Test
+    public void getTasks() {
+        securityUtil.logInAs("yuangong");
+
+        Page<Task> tasks = taskRuntime.tasks(Pageable.of(0,100));
+        List<Task> list=tasks.getContent();
+        for(Task tk : list){
+            System.out.println("-------------------");
+            System.out.println("getId："+ tk.getId());
+            System.out.println("getName："+ tk.getName());
+            System.out.println("getStatus："+ tk.getStatus());
+            System.out.println("getCreatedDate："+ tk.getCreatedDate());
+            if(tk.getAssignee() == null){
+                //候选人为当前登录用户，null的时候需要前端拾取
+                System.out.println("Assignee：待拾取任务");
+            }else{
+                System.out.println("Assignee："+ tk.getAssignee());
+            }
+
+        }
+    }
+
+    //完成任务
+    @Test
+    public void completeTask() {
+        securityUtil.logInAs("yuangong");
+        Task task = taskRuntime.task("bbc2ad6c-453e-11ed-a679-a036bc09649b");
+        if(task.getAssignee() == null){
+            taskRuntime.claim(TaskPayloadBuilder.claim()
+                    .withTaskId(task.getId())
+                    .build());
+        }
+        taskRuntime.complete(TaskPayloadBuilder
+                .complete()
+                .withTaskId(task.getId())
+                .build());
+        System.out.println("任务执行完成");
+    }}
